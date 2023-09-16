d5a97f8b006b1a1c5d3f0c7dad6c41f74e20c04e
Ceifly <107297079+ceifly@users.noreply.github.com>
2023-09-17 07:26:21 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,5 +1,5 @@
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=30417148&pid=d8208a89dcb51ed7ff9bdc35913fa457&tid=7e4c184bc92cb7f4de7d50f348b02fa4",
+    "https://www.maimemo.com/share/page?uid=30417148&pid=f87fe5203c433e2fca9b6ea8e5dd48da&tid=3b8b8b536732251017de8030d48ae89e",
   ]
   /**
    * 生成随机数字
